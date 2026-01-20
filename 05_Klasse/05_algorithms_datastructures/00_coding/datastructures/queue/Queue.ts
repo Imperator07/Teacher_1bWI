d8b4b67779f3b1e56205207ffe835b14cfae7b45
Imperator07 <samuel.amann.07@gmail.com>
2026-01-20 09:07:46 +0100

    init

--- 05_Klasse/05_algorithms_datastructures/00_coding/datastructures/queue/Queue.ts
+++ 05_Klasse/05_algorithms_datastructures/00_coding/datastructures/queue/Queue.ts
@@ -1,1 +1,20 @@
-class Queue {}
+import { Node } from "../Node.ts";
+
+export class Queue {
+    lastNode: Node | null
+
+    constructor() {
+        this.lastNode == null;
+    }
+
+    public enqueue() {
+        
+    }
+    public dequeue() {
+
+    }
+    public size() {
+
+    }
+
+}
